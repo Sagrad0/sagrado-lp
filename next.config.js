2d040a8addedb6e9826fd8f281f556a19583c7f6
Sagrad0 <sagrado203@gmail.com>
2025-11-25 21:17:30 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configurações básicas e estáveis
   compress: true,
-  
-  // Configuração mínima para imagens
   images: {
     formats: ['image/webp'],
-    minimumCacheTTL: 31536000, // 1 ano
+    minimumCacheTTL: 31536000, // 1 ano
   },
 }
 
